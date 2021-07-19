bf2faec3c4246d29e47c14953405679e540e6db8
Angry Coding <mailidanielifechukwu@gmail.com>
2021-07-19 11:15:03 +0100

    chnages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
 import React, { useRef } from "react";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
+import "./css/normalize.css";
+import "./css/webflow.css";
+import "./css/yoms.webflow.css";
 import FaceWebCam from "./components/faceWebCam";
+import ApiLogin from "./screens/ApiLogin";
+import ApiSignUp from "./screens/ApiSignUp";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import SignUp from "./screens/SignUp";
+import Success from "./screens/Success";
 
 function App() {
   return (
     <div className="App">
-      <FaceWebCam />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/success/:/:userImage" component={Success} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signUp" component={SignUp} />
+        <Route exact path="/api/login" component={ApiLogin} />
+        <Route exact path="/api/signUp" component={ApiSignUp} />
+      </Switch>
     </div>
   );
 }
